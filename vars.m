29d3ee70eea86be3a1730e9e0a6789ba855fb9b3
Tommy Zhang <tommyz2005@gmail.com>
2024-12-29 01:56:03 -0500

    add 6dof dynamics and pid

--- vars.m
+++ vars.m
@@ -1,17 +1,32 @@
 % environment
 g = 9.807;
+% Contact model
+contact.translation.spring = 3100;
+contact.translation.damper = 100;
+contact.translation.friction = 0.5;
+contact.translation.vd = 0.02;
+contact.translation.maxFriction = 20;
+contact.translation.maxNormal = 80;
+
+contact.rotation.spring = 2;
+contact.rotation.damper = 1;
+contact.rotation.friction = 0.03;
+control.rotation.maxMoment = 0.1;
+control.rotation.friction = 0.025;
+control.rotation.vd = 0.2;
 
 % rocket dynamics
-m = 1;
-r = 0.1;
-h = 0.3;
+m = 0.472; % kg
+r = 0.0381; % m
+h = 0.4; % m
+com = 0.215; % m
 
 armTVC = [
+    0.01
     0
-    0
-    0.4];
+    com-0.068];
 
-I_xx = (1/12) * m * (3*r^2 + h^2);
+I_xx = (1/12) * m * (3*r^2 + h^2) + m * com^2; % inertia as measured by pitch and roll
 I_yy = I_xx; 
 I_zz = (1/2) * m * r^2;
 
@@ -22,3 +37,5 @@ inertia = [I_xx, 0,    0;
 
 % rocket aerodynamics
 c_d = 0.2;
+
+sample_time = 0.1;
