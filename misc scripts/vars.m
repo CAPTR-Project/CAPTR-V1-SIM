7ecc9d08a62edc252ec20f1d1701380ed6e35249
Tommy Zhang <tommyz2005@gmail.com>
2024-12-31 00:58:53 -0500

    update pid implementation

--- misc scripts/vars.m
+++ misc scripts/vars.m
@@ -16,19 +16,21 @@ control.rotation.friction = 0.025;
 control.rotation.vd = 0.2;
 
 % rocket dynamics
-m = 0.472 + 0.08; % kg
-r = 0.0381; % m
+m = 0.544; % kg
+r_o = 0.0399; % m
+r_i = 0.0381;
 h = 0.4; % m
 com = 0.215; % m
 
 armTVC = [
     0
-    0.005
-    com-0.068];
+    -0.001
+    0.1064]; 
 
-I_xx = (1/12) * m * (3*r^2 + h^2) + m * com^2; % inertia as measured by pitch and roll
+% I_xx = (1/12) * m * (3*(r_o^2- r_i^2) + h^2) + m * com^2;% inertia as measured by pitch and roll
+I_xx = 0.00628;
 I_yy = I_xx; 
-I_zz = (1/2) * m * r^2;
+I_zz = (1/2) * m * r_o^2;
 
 inertia = [I_xx, 0,    0;
      0,    I_yy, 0;
@@ -38,7 +40,7 @@ inertia = [I_xx, 0,    0;
 % rocket aerodynamics
 c_d = 0.2;
 
-sample_time = 0.0025;
+sample_time = 1/416;
 alpha_rate = 1;
 alpha_att = 1;
 max_angle = 8*pi/180;
